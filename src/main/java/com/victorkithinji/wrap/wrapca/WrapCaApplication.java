7e7a4e856ba2d382610633d5f63168bbcf615a36
victorcodebase <victorkithinji@outlook.com>
2026-03-07 07:27:37 +0300

    Project  Initialize

---
+++ src/main/java/com/victorkithinji/wrap/wrapca/WrapCaApplication.java
@@ -0,0 +1,13 @@
+package com.victorkithinji.wrap.wrapca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapCaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrapCaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victorkithinji/wrap/wrapca/WrapCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorkithinji.wrap.wrapca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapCaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
